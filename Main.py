1a0096e379fcfbeedeefbb1829ef9996e9a08043
RasmusCC <8404428+rasmuscc@users.noreply.github.com>
2024-01-21 22:59:17 +0100

    Cleanup file- and field-names

--- Main.py
+++ Main.py
@@ -1,7 +1,9 @@
-from sss import SSS
+
+from secret_sharing import ShamirSecretSharing
+
 
 if __name__ == '__main__':
-    s = SSS()
+    s = ShamirSecretSharing()
     shares = s.create_shares(secret=1337, n=20, threshold=8)
     print("Shares: " + str(shares))
     print("Reconstructed secret: " + str(s.reconstruct_secret(shares, threshold=8)))
